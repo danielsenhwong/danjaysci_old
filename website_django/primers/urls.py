1889d4f5ebdb2e3af53d8c223920134743f58730
Daniel Wong <me@danielsenhwong.com>
2014-08-08 16:13:17 -0400

    changed formatting of comments in urls.py
    so switch from primer_id to pk for generic views

--- website_django/primers/urls.py
+++ website_django/primers/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, url
 from primers import views
 
 urlpatterns = patterns('',
-  # /primers/
-  url(r'^$', views.IndexView.as_view(), name='index'),
-  # /primers/1/
-  url(r'^(?P<primer_id>\d+)/$', views.DetailView.as_view(), name='detail'),
-  # /primers/1/edit/
-  url(r'^(?P<primer_id>\d+)/edit/$', views.edit, name='edit'),
+  url(r'^$', views.IndexView.as_view(), name='index'), # /primers/
+  url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'), # /primers/1/
+  url(r'^(?P<primer_id>\d+)/edit/$', views.edit, name='edit'),  # /primers/1/edit/
+
 )
