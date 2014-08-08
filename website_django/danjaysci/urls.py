2a6cf2560f2ea82555e7bbc6cba6439090347b84
Daniel Wong <me@danielsenhwong.com>
2014-08-08 18:28:42 -0400

    home index added

--- website_django/danjaysci/urls.py
+++ website_django/danjaysci/urls.py
@@ -8,7 +8,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'danjaysci.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # enable admin
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^primers/', include('primers.urls', namespace='primers')),
+
+    # admin documentation
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # my applications
+    url(r'^$', 'danjaysci.views.home', name='home'), # home
+    url(r'^primers/', include('primers.urls', namespace='primers')), # primers
 
 )
--- website_django/primers/tests.py
+++ website_django/primers/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
+from pimers.models import Primer
+
 # Create your tests here.
+class PrimerMethodTests(TestCase):
+  
