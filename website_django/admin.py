e5d7f42dfb47a26fd4e3b4d0b3b8d6af6a0eaad9
Daniel Wong <me@danielsenhwong.com>
2014-08-09 08:12:33 -0400

    corrected admin.py issue

--- website_django/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from pimers.models import LabMember
-
-admin.site.register(LabMember)
--- website_django/lab_members/admin.py
+++ website_django/lab_members/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from lab_members.models import LabMember
 
 # Register your models here.
+admin.site.register(LabMember)
