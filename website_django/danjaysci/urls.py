e4ab300ba0697d2ac1456bb8052ea335c4e74321
Daniel Wong <me@danielsenhwong.com>
2014-09-03 15:44:09 -0400

    edited base html, created protocols model

--- website_django/danjaysci/urls.py
+++ website_django/danjaysci/urls.py
@@ -14,6 +14,9 @@ urlpatterns = patterns('',
     # admin documentation
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    # user authentication
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
     # my applications
     url(r'^$', include('home.urls')), # home
     url(r'^references/', include('home.urls')), # references
